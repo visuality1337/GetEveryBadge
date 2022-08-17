96a58491d28804155fd5ce55ccd09da52d9888e2
Visual <68449061+Visual9999@users.noreply.github.com>
2022-08-17 11:56:27 +0200

    Update geteverybadge.plugin.js

--- geteverybadge.plugin.js
+++ geteverybadge.plugin.js
@@ -3,10 +3,10 @@
  * @version 1.1.1
  * @description Applies you all profile badges on Discord.
  * @website https://github.com/Visual9999
+ * @authorLink https://github.com/Visual9999
  * @source https://github.com/Visual9999/GetEveryBadge
  * @updateUrl https://raw.githubusercontent.com/Visual9999/GetEveryBadge/main/geteverybadge.plugin.js
  */
-
 class GetEveryBadge {
 
     constructor() {
@@ -24,7 +24,7 @@ class GetEveryBadge {
     }
 
     start() {
-        window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().flags = -1;}if (m.getCurrentUser !== undefined) {return m.getCurrentUser().flags = -1}}}]);window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().public_flags += 1;}if (m.getCurrentUser !== undefined) {return m.getCurrentUser().public_flags -= 33}}}])
+        window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().flags = -1;}if (m.getCurrentUser !== undefined) {return m.getCurrentUser().flags = -1}}}]);window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().public_flags += 1;}if (m.getCurrentUser !== undefined) {return m.getCurrentUser().public_flags += 1}}}])
     }
 
     stop() {
